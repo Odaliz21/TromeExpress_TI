0e03265e15b65108566c093c5d407ed8dcf40dc0
Odaliz Castro <75827085@continental.edu.pe>
2024-09-30 11:35:20 -0500

    frontend-api
    Se realiza la primera version y como interactua con la api

--- backend-api/index.js
+++ backend-api/index.js
@@ -25,7 +25,7 @@ app.use('/api/clientes', clientRoutes);
 // Rutas de Oficina 
 app.use('/api/oficinas', officeRoutes);
 // Puerto de escucha
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
